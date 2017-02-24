1f1468725a8eb0820f57acc7bd81db89cc9f3574
zws9310744 <105021108@live.asia.edu.tw>
2017-02-24 14:57:44 +0800

    hk submit

---
+++ src/hw00.java
@@ -0,0 +1,14 @@
+/*Topic:第一次作業自我介紹
+ * Date:2017/2/24
+ * Author:105021108 張惟舜
+ */
+public class hw00 {
+
+	public static void main(String[] args) {
+		System.out.println("張惟舜");
+		System.out.println("105021108");
+		System.out.println("資工 資電1A");
+
+	}
+
+}
